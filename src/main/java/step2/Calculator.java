9f2cf707321a29d103cd3fe79fbe5f07463132f2
정재우 <jjwoo@corealism.com>
2023-05-17 16:30:34 +0900

    jungHan's step2 push (주석 추가)

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -8,12 +8,17 @@ public class Calculator {
     - 숫자와 연산기호 사이에는 빈 공백 문자열
      */
     public static int calculate(String value) {
+        // 입력 값이 null이거나 빈 공백일 경우 예외를 발생
         if(Objects.isNull(value) || value.trim().isEmpty()){
             throw new IllegalArgumentException("Input value is null or space ");
         }
+        // 입력 값을 공백을 기준으로 분리
         String[] splitData = value.split(" ");
+
+        // 첫 번째 숫자를 변환하고 저장
         int result = myParseInt(splitData[0]);
 
+        // 두 번째 숫자부터 시작해서 연산자와 함께 계산
         for (int idx = 2; idx < splitData.length; idx += 2) {
             int operand = myParseInt(splitData[idx]);
             String operator = splitData[idx - 1];
@@ -22,7 +27,7 @@ public class Calculator {
 
         return result;
     }
-
+    // 문자열 정수로 변환
     private static int myParseInt(String str){
         try{
             return Integer.parseInt(str);
@@ -31,6 +36,7 @@ public class Calculator {
             throw new IllegalArgumentException("숫자가 아닐 경우", e);
         }
     }
+    //유효하지 않은 연산자일 경우 예외를 발생
     private static int calculateByOperator(int result, String operator, int operand) {
         switch (operator) {
             case "+":
@@ -59,6 +65,7 @@ public class Calculator {
         return a * b;
     }
 
+    //0으로 나눴을때 예외 발생
     private static int divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Divide by zero error.");
