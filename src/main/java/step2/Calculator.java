d30b92709fd17fd6d4d6f5b760713cdea07bb7ff
정재우 <jjwoo@corealism.com>
2023-05-19 15:59:34 +0900

    jungHan's step2 push (switch 변경, 로그 수정, null값과 빈 값 한번에)

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -1,12 +1,17 @@
 package step2;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.BinaryOperator;
 
-public class Calculator {
-    /*
+/*
     - 사용자 입력 문자열 값에따라 사칙연산 수행
     - 숫자와 연산기호 사이에는 빈 공백 문자열
      */
+public class Calculator {
+
+
     public static int calculate(String value) {
         // 입력 값이 null이거나 빈 공백일 경우 예외를 발생
         if(Objects.isNull(value) || value.trim().isEmpty()){
@@ -27,7 +32,7 @@ public class Calculator {
 
         return result;
     }
-    // 문자열 정수로 변환
+    /** 문자열 정수로 변환 **/
     private static int myParseInt(String str){
         try{
             return Integer.parseInt(str);
@@ -36,41 +41,66 @@ public class Calculator {
             throw new IllegalArgumentException("숫자가 아닐 경우", e);
         }
     }
-    //유효하지 않은 연산자일 경우 예외를 발생
+/**연산자의 값이 Hash에 없을 경우 예외발생
+
+    BinaryOperator는 Function의 특수한 형태
+    두 개의 같은 타입의 입력을 받아서 같은 타입의 결과를 반환하는 함수
+    BinaryOperator<Integer>의 apply 메서드는 두 개의 Integer를 받아서 Integer 결과를 반환
+해당 연산자에 대한 result 값을 계산한다.
+     **/
     private static int calculateByOperator(int result, String operator, int operand) {
-        switch (operator) {
-            case "+":
-                return add(result, operand);
-            case "-":
-                return subtract(result, operand);
-            case "*":
-                return multiply(result, operand);
-            case "/":
-                return divide(result, operand);
-            default:
-                throw new IllegalArgumentException("Invalid operator: " + operator);
+        if (!operators.containsKey(operator)) {
+            throw new IllegalArgumentException("Invalid operator: " + operator);
         }
-
+        return operators.get(operator).apply(result, operand);
+    }
+    private static Map<String, BinaryOperator<Integer>> operators = new HashMap<>();
+    static  {
+        operators.put("+", (a, b) -> a + b);
+        operators.put("-", (a, b) -> a - b);
+        operators.put("*", (a, b) -> a * b);
+        operators.put("/", (a, b) -> {
+            if (b == 0) {
+                throw new IllegalArgumentException("Divide by zero error.");
+            }
+            return a / b;
+        });
     }
 
-    private static int add(int a, int b) {
-        return a + b;
-    }
-
-    private static int subtract(int a, int b) {
-        return a - b;
-    }
-
-    private static int multiply(int a, int b) {
-        return a * b;
-    }
-
-    //0으로 나눴을때 예외 발생
-    private static int divide(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Divide by zero error.");
-        }
-        return a / b;
-    }
+//    private static int add(int a, int b) {
+//        return a + b;
+//    }
+//
+//    private static int subtract(int a, int b) {
+//        return a - b;
+//    }
+//
+//    private static int multiply(int a, int b) {
+//        return a * b;
+//    }
+//
+//    //0으로 나눴을때 예외 발생
+//    private static int divide(int a, int b) {
+//        if (b == 0) {
+//            throw new IllegalArgumentException("Divide by zero error.");
+//        }
+//        return a / b;
+//    }
+//    유효하지 않은 연산자일 경우 예외를 발생
+//    private static int calculateByOperator(int result, String operator, int operand) {
+//        switch (operator) {
+//            case "+":
+//                return add(result, operand);
+//            case "-":
+//                return subtract(result, operand);
+//            case "*":
+//                return multiply(result, operand);
+//            case "/":
+//                return divide(result, operand);
+//            default:
+//                throw new IllegalArgumentException("Invalid operator: " + operator);
+//        }
+//
+//    }
 
 }
--- src/test/java/step2/Step2Test.java
+++ src/test/java/step2/Step2Test.java
@@ -7,24 +7,24 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Step2Test {
 
-    /*  Test Method
+    /**  Test Method
     조건 1. 정수
     2. 입력 문자열의 숫자와 사칙 연산 사이에는 반드시 빈 공백 문자열이 있다고 가정한다.
 3. 사용자가 입력한 문자열 값에 따라 사칙연산을 수행할 수 있는 계산기를 구현해야 한다.
-    */
+    **/
     @Test
     @DisplayName("입력한 문자열 값에 따라 사칙연산을 수행할 수 있는 계산기를 구현")
     public void case1(){
         assertThat(Calculator.calculate("1 + 2 * 4")).isEqualTo(4);
     }
 
-    /*
+    /**
     1. 문자열 계산기는 사칙연산의 계산 우선순위가 아닌 입력 값에 따라 계산 순서가 결정된
 다. 즉, 수학에서는 곱셈, 나눗셈이 덧셈, 뺄셈 보다 먼저 계산해야 하지만 이를 무시한
 다.
     2. 예를 들어 2 + 3 * 4 / 2 와 같은 문자열을 입력할 경우 2 + 3 * 4 / 2 실행 결과인 10
 을 출력해야 한다.
-     */
+     **/
     @Test
     @DisplayName("2 + 3 * 4 / 2 실행 결과 10")
     public void case2(){
@@ -32,9 +32,9 @@ public class Step2Test {
     }
 
 
-    /*
+    /**
     사칙연산 기호가 아닌 경우 IllegalArgumentException throw
-     */
+     **/
     @Test
     @DisplayName("사칙연산 기호가 아닌 경우")
     public void exceptionCase1(){
@@ -44,9 +44,9 @@ public class Step2Test {
 
     }
 
-    /*
+    /**
     입력 값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException throw
-     */
+     **/
     @Test
     @DisplayName("입력 값이 null이거나 빈 공백 문자일 경우")
     public void exceptionCase2(){
@@ -61,9 +61,17 @@ public class Step2Test {
             Calculator.calculate(null);
         }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("@Test 입력 값이 null이거나 빈 공백 문자일 경우");
     }
-    /*
+    @Test
+    @DisplayName("한번에")
+    public void exceptionCase3_1(){
+        assertThatThrownBy(() -> {
+            Calculator.calculate("");
+            Calculator.calculate(null);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("@Test 입력 값이 null이거나 빈 공백 문자일 경우 한 번에");
+    }
+    /**
     입력 값을 0으로 나눌 경우
-     */
+     **/
     @Test
     @DisplayName("입력 값을 0으로 나눌 경우")
     public void exceptionCase4(){
@@ -71,9 +79,9 @@ public class Step2Test {
             Calculator.calculate("4 / 0");
         }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("@Test 입력 값을 0으로 나눌 경우");
     }
-    /*
+    /**
      숫자가 아닐 경우
-     */
+     **/
     @Test
     @DisplayName("숫자가 아닐 경우")
     public void exceptionCase5(){
